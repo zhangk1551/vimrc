1f4db97139066aec1609629c523e6858c79c2725
zhangk1551 <zhangk1551@gmail.com>
2021-10-03 03:08:12 +0000

    updates

--- update.sh
+++ update.sh
@@ -7,8 +7,7 @@ if [ `basename "$PWD"` != "vimrc" ]; then
 fi
 
 git submodule update --recursive --remote
-cp colors/base16/colors/base16-default-dark.vim colors/base16-default-dark.vim
 
-wget -O https://raw.githubusercontent.com/neoclide/coc.nvim/release/data/schema.json
+wget -O schema.json https://raw.githubusercontent.com/neoclide/coc.nvim/release/data/schema.json
 # By default don't reinstall LeaderF here, so some C extension updates in
 # LeaderF may be ignored. Run "./install.sh" under LeaderF directory if needed.
